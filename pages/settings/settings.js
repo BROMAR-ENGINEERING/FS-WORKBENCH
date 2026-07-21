a77eaeea2a7c26be2c6363ed56c179d2ec4015e5
Bromar <271488771+BROMAR-ENGINEERING@users.noreply.github.com>
2026-07-21 14:06:59 +1000

    Update settings.js

--- pages/settings/settings.js
+++ pages/settings/settings.js
@@ -13,6 +13,7 @@ SH.registerPage('settings', SH.tabbedPage({
   tabs: [
     { id: 'company', label: 'Company', src: 'pages/settings/tabs/company/company.js' },
     { id: 'report-theme', label: 'Report Theme', src: 'pages/settings/tabs/report-theme/report-theme.js' },
+    { id: 'table-style',  label: 'Table Style',  src: 'pages/settings/tabs/table-style/table-style.js' },
     { id: 'report-layout', label: 'Report Layout', src: 'pages/settings/tabs/report-layout/report-layout.js' },
     { id: 'information-sections', label: 'Sections', src: 'pages/settings/tabs/information-sections/information-sections.js' },
     { id: 'libraries', label: 'Component Libraries', src: 'pages/settings/tabs/libraries/libraries.js' },
